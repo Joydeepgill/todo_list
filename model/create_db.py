2b9eb03879eb8b50985c8fc9339770895cceb774
Joydeep Gill <joyydeep.gill@gmail.com>
2024-01-18 21:58:27 -0500

    Creating a dummy database

--- model/create_db.py
+++ model/create_db.py
@@ -7,4 +7,19 @@ connection = sqlite3.connect('tasks.db')
 cursor = connection.cursor()  
 
 
-cursor.execute("CREATE TABLE task(Id, User, Task, Priority, Status)")
+cursor.execute("CREATE TABLE task(Id integer, User text, Task text, Priority text, Status text)") 
+
+
+cursor.execute(""" 
+    INSERT INTO task VALUES
+         (1, 'Jenny', 'Vaccum', 'Med', 'In Progress'), 
+         (2, 'Abby', 'Clean dishes', 'High', 'Complete')
+""")  
+
+
+#print rows of the task table 
+for row in cursor.execute("SELECT * FROM task"):
+    print(row)
+
+
+connection.close() 
